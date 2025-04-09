3d9c80dd85a95696dcf174926f4f44275ee870d0
kzaldasp <kevinaldas290621@gmail.com>
2025-04-08 21:07:07 -0500

    asddsa

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    base: '/astro-pollo/', // 👈 nombre de tu repositorio
+    base: '/amormio/', // 👈 nombre de tu repositorio
     outDir: './dist', // esto ya debe estar por defecto
 });

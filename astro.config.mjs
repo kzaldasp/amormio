39c747dbe1623bbad5399b98191a37835e18c9ea
kzaldasp <kevinaldas290621@gmail.com>
2025-04-08 20:35:00 -0500

    Proyecto Astro Pollo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    base: '/amormio/', // 👈 nombre de tu repositorio
+    outDir: './dist', // esto ya debe estar por defecto
+});
